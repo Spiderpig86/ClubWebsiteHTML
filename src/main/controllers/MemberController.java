492961bb5ca61e7a51a27883c780a52ae788f650
Hid <mikeygulati@gmail.com>
2016-10-30 03:16:26 -0400

    edited Member pojo + integrated lombok to remove boilerplate code (please install lombok plugin if you use intellij)

--- src/main/controllers/MemberController.java
+++ src/main/controllers/MemberController.java
@@ -1,7 +1,36 @@
 package main.controllers;
 
+import lombok.Getter;
+import lombok.Setter;
+import main.services.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by me on 10/28/2016.
  */
+
+@Controller
 public class MemberController {
+    @Getter @Setter @Autowired
+    private MemberService memberService;
+
+    public MemberController() {
+        this.memberService = new MemberService();
+    }
+
+    @RequestMapping(value = "/members")
+    public String getMemberListHTML(Model model) {
+        //model.addAttribute("memberList", memberService.getMembers());
+        return "/members";
+    }
+
+    @RequestMapping(value = "/member/{id}")
+    public String getMemberHTML(@PathVariable Integer id, Model model) {
+        //model.addAttribute("member", memebrService.getMemberById(id));
+        return "/member";
+    }
 }
--- src/main/modelpojos/Member.java
+++ src/main/modelpojos/Member.java
@@ -1,10 +1,34 @@
 package main.modelpojos;
 
-/**
- * Created by me on 10/28/2016.
- */
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.*;
+
 public class Member {
-	
-	public String name;
-	
+	@Getter @Setter
+	private String fullName, email, major, imagePath;
+
+	@Getter @Setter
+	private Integer id;
+
+	@Getter
+	private final Set<Project> projects;
+
+	@Getter
+	private final Map<String, String> urls;
+
+	public Member() {
+		this.projects = new HashSet<>();
+		this.urls = new HashMap<>();
+	}
+
+	public Member(String fullName, String email, String major, String imagePath) {
+        this.fullName = fullName;
+        this.email = email;
+        this.major = major;
+        this.imagePath = imagePath;
+        this.projects = new HashSet<>();
+        this.urls = new HashMap<>();
+    }
 }
