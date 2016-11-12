45c65874dcab187bcf8a5aa472d870d6d38dfc47
Daniel Eliasinski <danieleliasinski@msn.com>
2016-11-11 20:22:37 -0500

    Added HomeController for homepage.

---
+++ src/main/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package main.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/")
+    public String home() {
+		
+        return "index";
+    }
+}
