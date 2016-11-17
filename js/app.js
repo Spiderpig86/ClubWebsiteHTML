05bc924cf746a995797545ed78d1015595e6dc40
spiderpig86 <slim679975@gmail.com>
2016-11-16 23:04:11 -0500

    Added polygon effect to the about div and slightly updated the color scheme.

--- js/app.js
+++ js/app.js
@@ -2,11 +2,23 @@
 
 // Function to make the landing div the same height as the window using jQuery
 $("#landing").css({
-    'min-height' : $(window).height() // Sets the minimum height of the div as the browser height.
+    'height' : $("#bg").height() // Sets the minimum height of the div as the browser height.
 })
 
 $(window).on('resize', function() { // Make sure the window height updates along with the browser
     $("#landing").css({
-        'height' : $(window).height()
+        'height' : $("#bg").height()
     })
 })
+
+// Drop the website menu down when the user scrolls past the landing div
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() < $("#landing").height()) {
+    $('ul#menuList').addClass('hidden');
+      //updateMobileMenu(true);
+  } else {
+    $('ul#menuList').removeClass('hidden');
+  }
+    console.log($(window).scrollTop() + " " + $("#landing").height());
+});
