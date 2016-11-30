e646a7c3ee1cf1d7c81a28c8c16a1010243e4a49
spiderpig86 <slim679975@gmail.com>
2016-11-29 20:56:04 -0500

    Updated mobile css again

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
 // JS File for the website
 
-// Function to make the landing div the same height as the window using jQuery
-$("#landing").css({
-    'height' : $("#bg").height() // Sets the minimum height of the div as the browser height.
-})
+if ($("#bg").height() > 650) {
+  // Function to make the landing div the same height as the window using jQuery
+  $("#landing").css({
+      'height' : $("#bg").height() // Sets the minimum height of the div as the browser height.
+  })
+}
 
-$(window).on('resize', function() { // Make sure the window height updates along with the browser
-    $("#landing").css({
-        'height' : $("#bg").height()
-    })
-})
+  $(window).on('resize', function() { // Make sure the window height updates along with the browser
+      if ($("#bg").height() > 650) {
+        $("#landing").css({
+          'height' : $("#bg").height()
+      })
+    }
+    console.log($("#bg").height());
+  })
+
 
 // Drop the website menu down when the user scrolls past the landing div
 
